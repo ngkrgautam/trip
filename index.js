3283a7727bf7808b2f12e42fc3001a5ccc474340
Gautam kumar ray <108655624+ngkrgautam@users.noreply.github.com>
2024-10-06 14:04:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function ck(){
+    var array = 'agra';
+    var name = document.getElementById('city-list');
+    if(name == array){
+        alert("Superb! Please wait, i find best plan for you.");
+    }
+    else {
+        alert("sorry! This is not listed.");
+    }
+}
